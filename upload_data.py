69ca587bf136ada45cab11e4f25f9f3f629a7168
chez8990 <chez8990@gmail.com>
2017-08-19 20:53:20 +0800

    making azure call with upload

--- upload_data.py
+++ upload_data.py
@@ -1,12 +1,13 @@
 import datetime
 import numpy as np
 import pandas as pd
+import os
 from calendar import monthrange
 
-lol = pd.read_csv('product_upload.csv')
 
 
-
+for filename in os.listdir('./Images'):
+	os.rename('./Images/'+filename,'./Images/'+ filename.split('_')[-1])
 def seasonal_split():
 
 	seasons = ['Spring','Summer','Fall','Winter']
@@ -25,43 +26,43 @@ def random_date(year, month):
 
 	return datetime.date(year, month, np.random.randint(1,days_in_month+1))
 
-all_seasons = lol['season'].values
-unique_seasons = lol['season'].unique()
-season_split = seasonal_split()
+# all_seasons = lol['season'].values
+# unique_seasons = lol['season'].unique()
+# season_split = seasonal_split()
 
-upload_date = []
+# upload_date = []
 
-print(season_split, unique_seasons[:3])
+# print(season_split, unique_seasons[:3])
 
 
-for i in all_seasons:
+# for i in all_seasons:
 		
-	if i in unique_seasons[:3]:
-		this_date = random_date(np.random.choice([2016,2017]), np.random.choice(season_split[i])).isoformat()
-		upload_date.append(this_date)
-	elif i=='Winter':
-		this_date = random_date(2016, np.random.choice(season_split[i])).isoformat()
-		upload_date.append(this_date)
-	elif i=='Spring/Summer':
-		this_date = random_date(np.random.choice([2016,2017]), np.random.choice([4,7])).isoformat()
-		upload_date.append(this_date)
-	elif i=='Fall/Winter':
-		this_date = random_date(np.random.choice([2016]), np.random.choice([9,11])).isoformat()
-		upload_date.append(this_date)
-	else:
-		this_date = random_date(np.random.choice([2016]), np.random.randint(1,12)).isoformat()
-		upload_date.append(this_date)
+# 	if i in unique_seasons[:3]:
+# 		this_date = random_date(np.random.choice([2016,2017]), np.random.choice(season_split[i])).isoformat()
+# 		upload_date.append(this_date)
+# 	elif i=='Winter':
+# 		this_date = random_date(2016, np.random.choice(season_split[i])).isoformat()
+# 		upload_date.append(this_date)
+# 	elif i=='Spring/Summer':
+# 		this_date = random_date(np.random.choice([2016,2017]), np.random.choice([4,7])).isoformat()
+# 		upload_date.append(this_date)
+# 	elif i=='Fall/Winter':
+# 		this_date = random_date(np.random.choice([2016]), np.random.choice([9,11])).isoformat()
+# 		upload_date.append(this_date)
+# 	else:
+# 		this_date = random_date(np.random.choice([2016]), np.random.randint(1,12)).isoformat()
+# 		upload_date.append(this_date)
 
-ran_id = []
-prelim='0000'
-for i in range(lol.shape[0]):
-	temp = prelim[:5-len(str(i))]+str(i)
-	ran_id.append(temp)
+# ran_id = []
+# prelim='0000'
+# for i in range(lol.shape[0]):
+# 	temp = prelim[:5-len(str(i))]+str(i)
+# 	ran_id.append(temp)
 
-print(ran_id)
-lol['upload_date'] = upload_date
-lol['photoId'] = ran_id
-lol.to_csv('product_upload.csv')
+# print(ran_id)
+# lol['upload_date'] = upload_date
+# lol['photoId'] = ran_id
+# lol.to_csv('product_upload.csv')
 
 
 
