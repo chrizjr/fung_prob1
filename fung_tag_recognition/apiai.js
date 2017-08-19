d2f1e2b6f8d7f24bbdc79d126e44254a55edb66d
juwonlee1020 <juwonlee1020@gmail.com>
2017-08-19 19:00:38 +0800

    API Demo
    tagging, connected w/ api.ai

--- fung_tag_recognition/apiai.js
+++ fung_tag_recognition/apiai.js
@@ -1,13 +1,17 @@
 var apiai = require('apiai');
 
-var app = apiai("<your client access token>");
+var app = apiai("a0e3d49509684e11a150e8afff2022e8");
 
-var request = app.textRequest('Spring', {
+var request = app.textRequest('2017 Summer is here!', {
     sessionId: '2'
 });
 
 request.on('response', function(response) {
     console.log(response);
+});
+
+request.on('error', function(error) {
+    console.log(error);
 });
 
 request.on('error', function(error) {
--- fung_tag_recognition/main.js
+++ fung_tag_recognition/main.js
@@ -4,33 +4,46 @@ imageLoader.addEventListener('change', handleImage, false);
 function handleImage(e) {
     var reader = new FileReader();
     reader.onload = function (event) {
-        $('.uploader img').attr('src',event.target.result);
+        $("#file_logo").hide();
+        $('#uploaded').show();
+        $('#uploaded').attr('src',event.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
 }
 
-var apiai = require('apiai');
-var app = apiai("a0e3d49509684e11a150e8afff2022e8");
-function sendRequest(e){
-    e.preventDefault();
-    var text_req = $("#d1").value();
-    var request = app.textRequest()
-    
+var accessToken = "a0e3d49509684e11a150e8afff2022e8";
+var baseUrl = "https://api.api.ai/v1/";
+
+$('input').on('itemAdded', function(event) {
+        event.preventDefault();
+        send();
+});
+function send(){
+    var text = $("#tags2").val();
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "query?v=20170819",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers:{
+            "Authorization": "Bearer" + accessToken
+        },
+        data: JSON.stringify({
+            query: text,
+            lang: "en",
+            sessionId: "random"
+        }),
+        success: function(data) {
+            setResponse(JSON.stringify(data.result.fulfillment.speech, undefined, 2));
+        }
+    })
+    function setResponse(val){
+        $(".col_names").text(val.slice(1, -1));
+        console.log(val);
+    }
 }
 
 
 
 
-var request = app.textRequest('Spring', {
-    sessionId: '2'
-});
 
-request.on('response', function(response) {
-    console.log(response);
-});
-
-request.on('error', function(error) {
-    console.log(error);
-});
-
-request.end();
